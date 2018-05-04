a9b8d8f113f345856a444bbb069dd2a09e40536b
Vitalii Antoniuk <va@jelasric.com>
2018-05-05 00:36:07 +0300

    bf wordpress

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,40 +1,70 @@
 <?php
-define('DB_NAME', 'DBNAME');
-define('DB_USER', 'DBUSER');
-define('DB_PASSWORD', 'DBPASS');
+/** Enable W3 Total Cache */
+define('WP_CACHE', false); // Added by WP Rocket
 
+define('DB_NAME', 'aloha_spa');
+define('DB_USER', 'aloha_spa');
+define('DB_PASSWORD', 'Fynjybj201cgf');
+
+/** MySQL hostname */
 define('DB_HOST', 'localhost');
+// define('DB_HOST', 'aloha.mysql.ukraine.com.ua');
 
-define('DISABLE_WP_CRON', true);
-define('FS_METHOD', 'direct');
-
-define('CACHE_READ_WHITELIST','_transient|posts WHERE ID IN|limit_login_'); // do not read from cache is sql contains these
-define('CACHE_WRITE_WHITELIST','_transient|limit_login_'); // do not reset cache if sql contains these
-
+/** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
-define('DB_COLLATE', 'utf8mb4_unicode_ci');
 
-$table_prefix  = 'rdhvp_';
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
 
-define('WPLANG', 'ru_RU');
-define('DISALLOW_FILE_EDIT', true);
-define('WP_DEBUG', false);
-define('WPCF7_AUTOP', false);
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'YQu^LML$f`iQ;B^BEde50Hg(OWGAl3}AOv*39]gxa.0!NXMj^YfE8v2R1b/yd;:9');
+define('SECURE_AUTH_KEY',  ':aNb=e}eoA?.YT hhyK#ti{x`Q^Y|:TIl~D?]zoX^7{=__iZD`(]W2T.y3F(BFw6');
+define('LOGGED_IN_KEY',    'iIGL01l<E{nDe=*0Vy5`ES)IN<RlLUkZj5Pb%$+,JGe/#I)%Pb{8]X%}8T`I:Du~');
+define('NONCE_KEY',        'Vr:!x^CR[ASLXp&2gEHN0Sg_GE7u4y|x4-dDkFQ{cI!P0lNz[TYf+g($5$*WB5$*');
+define('AUTH_SALT',        'vY5*;2=p[)9F7*wg5iz8?u$ibVx3(Q*;T%9mXUU^=pQN/y#miM#eQt;2{4P8]Gw2');
+define('SECURE_AUTH_SALT', '5Qv(A&t(/BL37=np;hL9k?;30QgJt @emYN)/ e[z@8uI!WN~ypUuywN)?&zx|@;');
+define('LOGGED_IN_SALT',   'ZnjY9e/GO4E,l[aK1} ZMcMb>~^`VkrDhZ!q4PP3N2XcrMhuTtWe7WsIu&n[#ba2');
+define('NONCE_SALT',       'wXyr!Pv/0HB+$S$3GJX?i1-nuWk&XZeDSwxxp5G$-sv|`z`n*`^dUIsNNhnm!W?F');
 
-define('AUTH_KEY',         'B]9_9_%uF{fdsasgC)pMx/?-+_bVjX;Xrib=1y23rgghdh3a+dadAEIZ1O/z^2Gv`<GLr<7hKI');
-define('SECURE_AUTH_KEY',  'Gasgb43@t+eWU&NhkNXw1daVO,adsa>mFU*kC^;8NAi0&;2RIz}a>:uO0[yU_0Cr<IPep&GG0U');
-define('LOGGED_IN_KEY',    'PvbNzyB^Z?fl|Kad..Du#4/|Y{iV|ntR22zndahar534L!k)T%~vU[5Tv4Vf*4D<m GXp#wAK_');
-define('NONCE_KEY',        'ubFTsbbd34Pf{Bi(ZU^QC!FM=.Qr*|id+i4#/Wvr[tasda~n+RYcs<5I8U+d:C%cb]|d]!|~R=');
-define('AUTH_SALT',        '/b2p2we%Gc-NSSxg]R2|P3=+m_*das5mq]a`vc<BZFfg12zsghjhn|^scLAJzF!U@1Lpx1yJhD');
-define('SECURE_AUTH_SALT', 'DGqahU{$#{1])WF?2d1{+v4mWhES6`o@))*asdaGcCa(t,+j~0+je]{`7fHc-=k!IC[U{1bjh-');
-define('LOGGED_IN_SALT',   '@a*]7xfnT!asd$-,Cw{~{Y~j38>jv!,]v%tr6jVRrH2:A)asrty3sg&56yuYZ=j+k>u@6`M|A}');
-define('NONCE_SALT',       '6jyK<[n:Wbnl)`;q2E:eVhp:[ez<+=|-xPadysegg5435g4n?WzGdEIfHqrFjeqV#zl|(oWv<4');
+/**#@-*/
 
-/* Это всё, дальше не редактируем. Успехов! */
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'aloha_';
 
-/** Абсолютный путь к директории WordPress. */
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('FORCE_SSL_ADMIN', true);
+define('WP_DEBUG', true);
+define( 'UPLOADS', ''.'img' );
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
-/** Инициализирует переменные WordPress и подключает файлы. */
+/** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
